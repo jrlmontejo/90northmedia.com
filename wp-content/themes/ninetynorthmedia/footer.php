ef268500f978446f0e4073ef5400c9b0a535cfa0
Jaye <jaye@whitewidget.com>
2017-01-25 02:12:40 +0800

    Fixed broken links on footer

--- wp-content/themes/ninetynorthmedia/footer.php
+++ wp-content/themes/ninetynorthmedia/footer.php
@@ -6,8 +6,12 @@
 				<?php echo sprintf( __( 'Copyright %1$s %2$s | %3$s | All Rights Reserved.', 'ninetynorthmedia' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 			</div>
 			<div class="mainFooter_social col-xs-12 col-md-2">
-				<i class="fa fa-facebook fa-2x"></i>
-				<i class="fa fa-linkedin-square fa-2x"></i>
+				<a href="//www.facebook.com/90degreesnorth/" target="_blank">
+					<i class="fa fa-facebook fa-2x"></i>
+				</a>
+				<a href="//www.linkedin.com/company/90-degrees-north-inc-" target="_blank">
+					<i class="fa fa-linkedin-square fa-2x"></i>
+				</a>
 			</div>
 		</footer>
 
