8f228c64d1f21908f181c009b111eca57c0f4e15
Jaye <jaye@whitewidget.com>
2017-01-25 03:33:14 +0800

    Initial footer design

--- wp-content/themes/ninetynorthmedia/footer.php
+++ wp-content/themes/ninetynorthmedia/footer.php
@@ -2,16 +2,30 @@
 		</div>
 		
 		<footer id="footer" class="mainFooter">
-			<div id="copyright" class="mainFooter_copy col-xs-12 col-md-10">
-				<?php echo sprintf( __( 'Copyright %1$s %2$s | %3$s | All Rights Reserved.', 'ninetynorthmedia' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+			<div class="mainFooter_contact">
+				<div class="mainFooter_contact_info mainFooter_contact_info_1">
+					<i class="fa fa-map-marker fa-lg fa-fw"></i>
+					<span>509 Emerald Court Bldg., 54 Road 3, QC, PH 1100</span>
+				</div>
+				<div class="mainFooter_contact_info mainFooter_contact_info_2">
+					<i class="fa fa-envelope fa-lg fa-fw"></i>
+					<span>info@90northmedia.com</span>
+				</div>
+				<div class="mainFooter_contact_info mainFooter_contact_info_3">
+					<i class="fa fa-phone fa-lg fa-fw"></i>
+					<span>(02) 959 6837</span>
+				</div>
 			</div>
-			<div class="mainFooter_social col-xs-12 col-md-2">
+			<div class="mainFooter_social">
 				<a href="//www.facebook.com/90degreesnorth/" target="_blank">
-					<i class="fa fa-facebook fa-2x"></i>
+					<i class="fa fa-facebook fa-lg fa-fw"></i>
 				</a>
 				<a href="//www.linkedin.com/company/90-degrees-north-inc-" target="_blank">
-					<i class="fa fa-linkedin-square fa-2x"></i>
+					<i class="fa fa-linkedin-square fa-lg fa-fw"></i>
 				</a>
+			</div>
+			<div id="copyright" class="mainFooter_copy">
+				<?php echo sprintf( __( 'Copyright %1$s %2$s | %3$s | All Rights Reserved.', 'ninetynorthmedia' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 			</div>
 		</footer>
 
